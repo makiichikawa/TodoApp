ff530007d47fc0c32bfa5219049ddad175e8f112
makiichikawa <punkfolkfolk@gmail.com>
2024-05-12 20:49:20 +0900

    タスク構造体を別のモジュールへ切り出し

--- TodoApp/ContentView.swift
+++ TodoApp/ContentView.swift
@@ -23,17 +23,6 @@ struct ContentView: View {
     }
 }
 
-struct Task: Identifiable {
-    let id = UUID()
-    var title: String
-    var checked: Bool
-
-    init(title: String, checked: Bool) {
-        self.title = title
-        self.checked = checked
-    }
-}
-
 #Preview {
     ContentView()
 }
