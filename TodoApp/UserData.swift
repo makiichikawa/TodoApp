049cdcffa79d298226929078d84611d2be6ae5d2
makiichikawa <punkfolkfolk@gmail.com>
2024-05-12 20:41:49 +0900

    tasksのデータを別モジュールに切り出し

---
+++ TodoApp/UserData.swift
@@ -0,0 +1,16 @@
+//
+//  UserData.swift
+//  TodoApp
+//
+//  Created by 市川真希 on 2024/05/12.
+//
+
+import SwiftUI
+
+struct UserData {
+    var tasks = [
+        Task(title: "散歩", checked: true),
+        Task(title: "料理", checked: false),
+        Task(title: "筋トレ", checked: true)
+    ]
+}
